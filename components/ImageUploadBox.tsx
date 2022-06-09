9a42b6c2420ff05495308408d7a4865dda1ce44e
Blankeos <carloantonioct@gmail.com>
2022-06-09 15:07:33 +0800

    Updated ImageUploadBox

--- components/ImageUploadBox.tsx
+++ components/ImageUploadBox.tsx
@@ -8,23 +8,18 @@ import { CgSpinner as SpinnerIcon } from "react-icons/cg";
 // Hooks
 import { useDropzone } from "react-dropzone";
 import toast from "react-hot-toast";
+import { imageUploadToasts } from "../services/customToasts";
 
-// API stuff
-import axios from "axios";
-
-interface SelectedFile extends File {
-  preview: string;
-}
-
-const ImageUploadBox = () => {
-  const [file, setFile] = useState<SelectedFile | undefined>();
-  const [loading, setLoading] = useState<boolean>(false);
-
+type ImageUploadBoxProps = {
+  file: SelectedFile | undefined;
+  setFile: React.Dispatch<React.SetStateAction<SelectedFile | undefined>>;
+};
+const ImageUploadBox: React.FC<ImageUploadBoxProps> = ({ file, setFile }) => {
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
     accept: "image/*",
     multiple: false,
     onDropAccepted: (acceptedFiles) => {
-      toast.success(`Successfully chose image!`);
+      imageUploadToasts.success();
       setFile(
         Object.assign(acceptedFiles[0], {
           preview: URL.createObjectURL(acceptedFiles[0]),
@@ -32,81 +27,45 @@ const ImageUploadBox = () => {
       );
     },
     onDropRejected: (fileRejection) => {
-      toast.error("Can't accept this file type.");
+      imageUploadToasts.error();
     },
   });
-
-  useEffect(() => {
-    console.log(file);
-  }, [file]);
-
-  async function handleUpload(e: React.MouseEvent<HTMLButtonElement>) {
-    // Null check "file" state
-    if (!file) {
-      // Display that no file is selected
-      return;
-    }
-    setLoading(true);
-    let formdata = new FormData();
-
-    formdata.append("image", file);
-    formdata.append("name", "Carlo Taleon");
-
-    await axios({
-      url: "http://localhost:8000/uploadfile",
-      method: "POST",
-      headers: {
-        authorization: "your token",
-      },
-      data: formdata,
-    }).then(
-      (res) => {
-        // Display success in POST request
-      },
-      (err) => {
-        // Display errors in POST request
-      }
-    );
-    setLoading(false);
-  }
-
   return (
     <>
       <input {...getInputProps()} />
-      <div className="max-w-5xl w-full mx-auto px-7 flex flex-col items-center">
-        <label
-          {...getRootProps()}
-          className="h-72 w-full bg-gray-200 rounded-md text-gray-400 cursor-pointer border border-gray-300 hover:shadow-inner transition relative overflow-hidden"
-          style={{
-            backgroundImage: `url('${file?.preview}')`,
-            backgroundPosition: "center",
-            backgroundSize: "contain",
-            backgroundRepeat: "no-repeat",
-          }}
+      <label
+        {...getRootProps()}
+        className="h-full w-full bg-gray-200 rounded-xl text-gray-400 cursor-pointer border border-gray-300 hover:shadow-inner transition relative overflow-hidden"
+        style={{
+          backgroundImage: `url('${file?.preview}')`,
+          backgroundPosition: "center",
+          backgroundSize: "contain",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        <div
+          className={`group absolute inset-0 transition bg-gray-900 ${
+            isDragActive ? "bg-opacity-80" : "bg-opacity-0"
+          } hover:bg-opacity-70`}
         >
           <div
-            className={`group absolute inset-0 transition bg-gray-900 ${
-              isDragActive ? "bg-opacity-80" : "bg-opacity-0"
-            } hover:bg-opacity-70`}
+            className={`w-full h-full flex justify-center items-center flex-col gap-5 transition group-hover:opacity-100 text-center ${
+              file
+                ? isDragActive
+                  ? "opacity-100"
+                  : "opacity-0"
+                : "opacity-100"
+            } `}
           >
-            <div
-              className={`w-full h-full flex justify-center items-center flex-col gap-5 transition ${
-                file
-                  ? isDragActive
-                    ? "opacity-100"
-                    : "opacity-0"
-                  : "opacity-100"
-              } group-hover:opacity-100`}
-            >
-              <UploadIcon />
-              <span>
-                <b className="font-semibold">Choose an image</b> or drag it
-                here.
-              </span>
-            </div>
+            <UploadIcon />
+            <span>
+              <b className="font-semibold">Upload a spine x-ray image</b> or
+              drag it here.
+            </span>
           </div>
-        </label>
-        <p className="text-xs my-3 text-gray-400 w-full text-center truncate">
+        </div>
+      </label>
+      {/* <p className="text-xs my-3 text-gray-400 w-full text-center truncate">
           {file ? file.name : <span className="invisible">.</span>}
         </p>
         <button
@@ -122,8 +81,7 @@ const ImageUploadBox = () => {
           <span className="text-center w-full">
             {loading ? "Measuring Cobb Angle..." : "Measure Cobb Angle"}
           </span>
-        </button>
-      </div>
+        </button> */}
     </>
   );
 };
