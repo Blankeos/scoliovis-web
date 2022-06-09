55ce1e20e46cdb7b2b2cd79b26207394e58e8edc
Blankeos <carloantonioct@gmail.com>
2022-06-09 15:06:40 +0800

    Created global types

---
+++ src/types.d.ts
@@ -0,0 +1,8 @@
+interface SelectedFile extends File {
+  preview: string;
+}
+// Custom Type for a React functional component with props AND CHILDREN
+// Source: https://stackoverflow.com/questions/71788254/react-18-typescript-children-fc/71800185#71800185 solution by ashuvssut
+// Import syntax in d.ts files: https://stackoverflow.com/questions/39040108/import-class-in-definition-file-d-ts
+type FCC<P = {}> = import("react").FC<import("react").PropsWithChildren<P>>;
+//
