0ee68c7b809c705eefbeb23f472db726106d3348
Blankeos <carloantonioct@gmail.com>
2022-08-05 23:50:17 +0800

    Edited serverIsBootingUp toast

--- src/toasts/serverIsBootingUp.tsx
+++ src/toasts/serverIsBootingUp.tsx
@@ -2,9 +2,14 @@ import toast from "react-hot-toast";
 
 const serverIsBootingUp = () => {
   const t = toast((t) => (
-    <span>
-      Custom and <b>bold</b>
-      <button onClick={() => toast.dismiss(t.id)}>Dismiss</button>
+    <span className="grid grid-cols-2">
+      <span className="text-4xl">😨</span>
+      <span className="flex flex-col">
+        <span className="font-bold">Model is taking too long...</span>
+        <span>It's most likely because Heroku is still starting up.</span>
+        <span>Please wait for a bit.</span>
+        {/* <button onClick={() => toast.dismiss(t.id)}>Dismiss</button> */}
+      </span>
     </span>
   ));
   return t;
