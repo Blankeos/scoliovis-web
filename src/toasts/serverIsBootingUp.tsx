ce3024121cfa571b8da55a78ddc3fc00085e5883
Blankeos <carloantonioct@gmail.com>
2022-08-06 00:00:35 +0800

    Fixed tailwind purge + finished serverIsBootingUp toast

--- src/toasts/serverIsBootingUp.tsx
+++ src/toasts/serverIsBootingUp.tsx
@@ -2,13 +2,16 @@ import toast from "react-hot-toast";
 
 const serverIsBootingUp = () => {
   const t = toast((t) => (
-    <span className="grid grid-cols-2">
-      <span className="text-4xl">😨</span>
+    <span className="flex gap-x-4">
+      <span className="text-3xl self-center animate-pulse">😭</span>
       <span className="flex flex-col">
-        <span className="font-bold">Model is taking too long...</span>
-        <span>It's most likely because Heroku is still starting up.</span>
-        <span>Please wait for a bit.</span>
-        {/* <button onClick={() => toast.dismiss(t.id)}>Dismiss</button> */}
+        <span className="font-bold mb-2 text-gray-800">
+          Model is taking too long...
+        </span>
+        <span className="text-gray-700">
+          Our <b className="font-semibold text-purple-500">Heroku</b> server
+          takes a while to start up. Please wait for a bit.
+        </span>
       </span>
     </span>
   ));
