d3b6ff4fdd385ab597dceefac45aba394ad32a27
Blankeos <carloantonioct@gmail.com>
2022-10-19 23:14:50 +0800

    tentative scroll to top with react-scroll

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,18 +1,35 @@
 import Link from "next/link";
 import React from "react";
+import { useRouter } from "next/router";
+
+import { animateScroll as scroll } from "react-scroll";
+// nav className="sticky top-0 bg-white z-20 backdrop-blur-sm bg-opacity-80"
 
 const Nav = () => {
+  const router = useRouter();
   return (
     <nav className="">
       <div className="fluid-container py-7 px-9 w-full overflow-hidden">
         <div className="flex justify-between gap-x-8">
-          <Link href="/">
-            <a>
-              <h1 className="text-2xl text-primary">
-                <span className="font-black">Scolio</span>Vis
-              </h1>
+          {router.pathname === "/" ? (
+            <a
+              onClick={(e) => {
+                e.preventDefault();
+                scroll.scrollToTop();
+              }}
+              className="text-2xl text-primary cursor-pointer"
+            >
+              <span className="font-black">Scolio</span>Vis
             </a>
-          </Link>
+          ) : (
+            <Link href="/">
+              <a className="cursor-pointer">
+                <h1 className="text-2xl text-primary">
+                  <span className="font-black">Scolio</span>Vis
+                </h1>
+              </a>
+            </Link>
+          )}
           <ul className="flex gap-x-8 overflow-hidden">
             <li>
               <Link href="/about">
