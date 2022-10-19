9f0ebd8512f51f44924499ab3d587f9839f769b8
Blankeos <carloantonioct@gmail.com>
2022-10-19 22:57:24 +0800

    Displayed author pictures on about and updated Head

--- pages/about.tsx
+++ pages/about.tsx
@@ -5,9 +5,12 @@ import { motion } from "framer-motion";
 
 import enterAnim from "@/utils/enterAnim";
 
+import Image from "next/image";
+import Head from "components/Head";
 const AboutPage = () => {
   return (
     <div className="flex flex-col min-h-screen">
+      <Head pageTitle="About" pagePath="about" />
       <Nav />
       <main className="flex-grow">
         <div className="w-full max-w-xl mx-auto px-9">
@@ -34,18 +37,47 @@ const AboutPage = () => {
           </h2>
           <div className="grid grid-cols-2 md:grid-cols-3 gap-x-10">
             <div className="grid grid-rows-2 justify-items-center gap-y-3 col-span-2 md:col-span-1">
-              <div className="w-[100px] h-[100px] rounded-full bg-gradient-to-br from-purple-400 to-primary"></div>
-              <h3 className="text-center max-w-[120px]">
+              <div className="relative w-[100px] h-[100px] grid place-items-center">
+                <div className="absolute w-[80px] h-[80px]  rounded-full bg-gradient-to-br from-purple-400 to-primary shadow-md shadow-blue-400" />
+                <Image
+                  alt="carlo as apex"
+                  src="/assets/apexcarlo.png"
+                  width={200}
+                  height={200}
+                  objectFit="contain"
+                />
+              </div>
+              <h3 className="text-gray-700 text-center max-w-[120px]">
                 Carlo Antonio T. Taleon
               </h3>
             </div>
             <div className="grid grid-rows-2 justify-items-center gap-y-3">
-              <div className="w-[100px] h-[100px] rounded-full bg-gradient-to-br from-purple-400 to-primary"></div>
-              <h3 className="text-center  max-w-[120px]">Glecy S. Elizalde</h3>
+              <div className="relative w-[100px] h-[100px] grid place-items-center">
+                <div className="absolute w-[80px] h-[80px]  rounded-full bg-gradient-to-br from-purple-400 to-primary shadow-md shadow-blue-400" />
+                <Image
+                  alt="glecy as apex"
+                  src="/assets/apexglecy.png"
+                  width={200}
+                  height={200}
+                  objectFit="contain"
+                />
+              </div>
+              <h3 className="text-gray-700 text-center max-w-[120px]">
+                Glecy S. Elizalde
+              </h3>
             </div>
             <div className="grid grid-rows-2 justify-items-center gap-y-3">
-              <div className="w-[100px] h-[100px] rounded-full bg-gradient-to-br from-purple-400 to-primary"></div>
-              <h3 className="text-center  max-w-[120px]">
+              <div className="relative w-[100px] h-[100px] grid place-items-center">
+                <div className="absolute w-[80px] h-[80px]  rounded-full bg-gradient-to-br from-purple-400 to-primary shadow-md shadow-blue-400" />
+                <Image
+                  alt="cj as apex"
+                  src="/assets/apexcj.png"
+                  width={200}
+                  height={200}
+                  objectFit="contain"
+                />
+              </div>
+              <h3 className="text-gray-700 text-center max-w-[120px]">
                 Christopher Joseph T. Rubinos
               </h3>
             </div>
@@ -54,9 +86,13 @@ const AboutPage = () => {
           <h2 className="mt-16 mb-6 text-2xl text-center text-gray-700 font-semibold">
             Special Thanks to
           </h2>
-          <p>Dr. Frank Elijorde</p>
-          <p>Dr. Bobby Gerardo</p>
-          <p>Dr. Shuo Li</p>
+          <p className="text-gray-700">
+            Dr. Frank I. Elijorde - Thesis Adviser
+          </p>
+          <p className="text-gray-700">
+            Dr. Bobby D. Gerardo - Thesis Co-Adviser
+          </p>
+          <p className="text-gray-700">Dr. Shuo Li - SpineWeb Dataset 16</p>
         </div>
       </main>
       <Footer />
