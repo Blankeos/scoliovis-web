50c5be0d2a5ea3df525b506d182e9840f7f4560e
Blankeos <carloantonioct@gmail.com>
2022-10-19 23:26:15 +0800

    Animation updates on about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -7,7 +7,12 @@ import enterAnim from "@/utils/enterAnim";
 
 import Image from "next/image";
 import Head from "components/Head";
+import useRefInView from "@/hooks/useRefInView";
+
 const AboutPage = () => {
+  const [ref1, inView1] = useRefInView();
+  const [ref2, inView2] = useRefInView();
+
   return (
     <div className="flex flex-col min-h-screen">
       <Head pageTitle="About" pagePath="about" />
@@ -32,11 +37,18 @@ const AboutPage = () => {
             salted walnut pesto flakes.
           </motion.p>
 
-          <h2 className="mt-16 mb-6 text-2xl text-center text-gray-700 font-semibold">
+          <motion.h2
+            ref={ref1}
+            {...enterAnim(0.3, inView1)}
+            className="mt-16 mb-6 text-2xl text-center text-gray-700 font-semibold"
+          >
             Authors of ScolioVis
-          </h2>
-          <div className="grid grid-cols-2 md:grid-cols-3 gap-x-10">
-            <div className="grid grid-rows-2 justify-items-center gap-y-3 col-span-2 md:col-span-1">
+          </motion.h2>
+          <div ref={ref2} className="grid grid-cols-2 md:grid-cols-3 gap-x-10">
+            <motion.div
+              {...enterAnim(0.4, inView2)}
+              className="grid grid-rows-2 justify-items-center gap-y-3 col-span-2 md:col-span-1"
+            >
               <div className="relative w-[100px] h-[100px] grid place-items-center">
                 <div className="absolute w-[80px] h-[80px]  rounded-full bg-gradient-to-br from-purple-400 to-primary shadow-md shadow-blue-400" />
                 <Image
@@ -50,8 +62,11 @@ const AboutPage = () => {
               <h3 className="text-gray-700 text-center max-w-[120px]">
                 Carlo Antonio T. Taleon
               </h3>
-            </div>
-            <div className="grid grid-rows-2 justify-items-center gap-y-3">
+            </motion.div>
+            <motion.div
+              {...enterAnim(0.6, inView2)}
+              className="grid grid-rows-2 justify-items-center gap-y-3"
+            >
               <div className="relative w-[100px] h-[100px] grid place-items-center">
                 <div className="absolute w-[80px] h-[80px]  rounded-full bg-gradient-to-br from-purple-400 to-primary shadow-md shadow-blue-400" />
                 <Image
@@ -65,8 +80,11 @@ const AboutPage = () => {
               <h3 className="text-gray-700 text-center max-w-[120px]">
                 Glecy S. Elizalde
               </h3>
-            </div>
-            <div className="grid grid-rows-2 justify-items-center gap-y-3">
+            </motion.div>
+            <motion.div
+              {...enterAnim(0.8, inView2)}
+              className="grid grid-rows-2 justify-items-center gap-y-3"
+            >
               <div className="relative w-[100px] h-[100px] grid place-items-center">
                 <div className="absolute w-[80px] h-[80px]  rounded-full bg-gradient-to-br from-purple-400 to-primary shadow-md shadow-blue-400" />
                 <Image
@@ -80,7 +98,7 @@ const AboutPage = () => {
               <h3 className="text-gray-700 text-center max-w-[120px]">
                 Christopher Joseph T. Rubinos
               </h3>
-            </div>
+            </motion.div>
           </div>
 
           <h2 className="mt-16 mb-6 text-2xl text-center text-gray-700 font-semibold">
