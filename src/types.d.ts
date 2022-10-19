7c8beed85a6d68582c8968057db50652439a0847
Blankeos <carloantonioct@gmail.com>
2022-10-20 00:40:52 +0800

    Renamed SelectedFile to ISelectedFile

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,6 +1,7 @@
-interface SelectedFile extends File {
+interface ISelectedFile extends File {
   preview: string;
 }
+
 // Custom Type for a React functional component with props AND CHILDREN
 // Source: https://stackoverflow.com/questions/71788254/react-18-typescript-children-fc/71800185#71800185 solution by ashuvssut
 // Import syntax in d.ts files: https://stackoverflow.com/questions/39040108/import-class-in-definition-file-d-ts
