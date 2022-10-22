b0c25c0c1c88b6ac54817a0948c6baebd61b4b8c
Blankeos <carloantonioct@gmail.com>
2022-10-23 00:16:28 +0800

    urlToSelectedFile complies with new types

--- services/urlToSelectedFile.ts
+++ services/urlToSelectedFile.ts
@@ -1,4 +1,7 @@
 // Source of this code: https://stackoverflow.com/questions/62179675/how-to-convert-image-source-into-a-javascript-file-object
+
+import getImageDataFromURL from "@/utils/getImageDataFromURL";
+
 // ***Here is the code for converting "image source" (url) to "Base64".***
 const getUrlExtension = (url: string) => {
   return url.split(/[#?]/)[0].split(".").pop()?.trim();
@@ -13,9 +16,16 @@ const urlToSelectedFile = async (imgUrl: string): Promise<ISelectedFile> => {
     type: blob.type,
   });
 
+  const src = URL.createObjectURL(file);
+  const { img, width, height } = await getImageDataFromURL(src);
+
+  // Create Image Bitmap
+
   return Object.assign(file, {
-    preview: URL.createObjectURL(file),
+    img: img,
+    src: src,
+    width: width,
+    height: height,
   });
 };
-
 export default urlToSelectedFile;
