18b9abb9dc7369c1f16c5b0652e1e3f43237e86e
Blankeos <carloantonioct@gmail.com>
2022-10-23 00:15:56 +0800

    Updated types (selected file, stores html img)

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,6 +1,22 @@
 interface ISelectedFile extends File {
-  preview: string;
+  img: HTMLImageElement;
+  src: string;
+  width: number;
+  height: number;
 }
+
+type DrawSettingsType = {
+  landmarkDisplayType: LandmarkDisplayType;
+  landmarkSize: number;
+  landmarkColor: [string, string];
+  lineWidth: number;
+};
+
+type LandmarkDisplayType =
+  | "no_lines"
+  | "top_lines"
+  | "bottom_lines"
+  | "all_lines";
 
 // Custom Type for a React functional component with props AND CHILDREN
 // Source: https://stackoverflow.com/questions/71788254/react-18-typescript-children-fc/71800185#71800185 solution by ashuvssut
