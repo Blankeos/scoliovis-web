ef5369152742846d920b3ef845c96cf647283151
Blankeos <carloantonioct@gmail.com>
2022-10-26 23:11:53 +0800

    Implemented immer middleware from zustand/middleware/immer

--- src/types.d.ts
+++ src/types.d.ts
@@ -18,6 +18,10 @@ type LandmarkDisplayType =
   | "bottom_lines"
   | "all_lines";
 
+type ScolioVisAPIResponseType = {
+  landmarks: number[];
+};
+
 // Custom Type for a React functional component with props AND CHILDREN
 // Source: https://stackoverflow.com/questions/71788254/react-18-typescript-children-fc/71800185#71800185 solution by ashuvssut
 // Import syntax in d.ts files: https://stackoverflow.com/questions/39040108/import-class-in-definition-file-d-ts
--- store/index.ts
+++ store/index.ts
@@ -1,10 +1,7 @@
 import create from "zustand";
 import { devtools } from "zustand/middleware";
+import { immer } from "zustand/middleware/immer";
 import produce from "immer";
-
-type ScolioVisAPIResponseType = {
-  landmarks: number[];
-};
 
 export interface IStoreState {
   //   States
@@ -27,45 +24,81 @@ export interface IStoreState {
 }
 
 export const useStore = create<IStoreState>()(
-  devtools((set) => ({
-    selectedFile: undefined,
-    drawSettings: {
-      landmarkDisplayType: "no_lines",
-      landmarkColor: ["#FFFFFF", "#8ED1FC"],
-      landmarkSize: 7,
-      lineWidth: 2,
-    },
-    scoliovisAPIResponse: undefined,
-    setScoliovisAPIResponse: (responseData) =>
-      set(
-        produce((state: any) => {
+  devtools(
+    immer((set) => ({
+      selectedFile: undefined,
+      drawSettings: {
+        landmarkDisplayType: "no_lines",
+        landmarkColor: ["#FFFFFF", "#8ED1FC"],
+        landmarkSize: 7,
+        lineWidth: 2,
+      },
+      scoliovisAPIResponse: undefined,
+      setScoliovisAPIResponse: (responseData) =>
+        set((state) => {
           state.scoliovisAPIResponse = responseData;
-        })
-      ),
-    setSelectedFile: (file) =>
-      set(
-        produce((state: IStoreState) => {
-          state.selectedFile = file;
-        })
-      ),
-    setLandmarkDisplayType: (displayType) =>
-      set(
-        produce((state: IStoreState) => {
+        }),
+      setSelectedFile: (file) =>
+        set((state) => {
+          state.selectedFile = file as any; // just to make the typescript compiler happy. (issue with the img type not being compatible. Too deep to fix, but works)
+        }),
+      setLandmarkDisplayType: (displayType) =>
+        set((state) => {
           state.drawSettings.landmarkDisplayType = displayType;
-        })
-      ),
-    setLandmarkSize: (size) =>
-      set(
-        produce((state: IStoreState) => {
+        }),
+      setLandmarkSize: (size) =>
+        set((state) => {
           state.drawSettings.landmarkSize = size;
-        })
-      ),
-    setLandmarkColor: ({ topColor, bottomColor }) =>
-      set(
-        produce((state: IStoreState) => {
+        }),
+      setLandmarkColor: ({ topColor, bottomColor }) =>
+        set((state) => {
           if (topColor) state.drawSettings.landmarkColor[0] = topColor;
           if (bottomColor) state.drawSettings.landmarkColor[1] = bottomColor;
-        })
-      ),
-  }))
+        }),
+    }))
+  )
 );
+
+// export const useStore = create<IStoreState>()(
+//   devtools((set) => ({
+//     selectedFile: undefined,
+//     drawSettings: {
+//       landmarkDisplayType: "no_lines",
+//       landmarkColor: ["#FFFFFF", "#8ED1FC"],
+//       landmarkSize: 7,
+//       lineWidth: 2,
+//     },
+//     scoliovisAPIResponse: undefined,
+//     setScoliovisAPIResponse: (responseData) =>
+//       set(
+//         produce((state: any) => {
+//           state.scoliovisAPIResponse = responseData;
+//         })
+//       ),
+//     setSelectedFile: (file) =>
+//       set(
+//         produce((state: any) => {
+//           state.selectedFile = file;
+//         })
+//       ),
+//     setLandmarkDisplayType: (displayType) =>
+//       set(
+//         produce((state: any) => {
+//           state.drawSettings.landmarkDisplayType = displayType;
+//         })
+//       ),
+//     setLandmarkSize: (size) =>
+//       set(
+//         produce((state: any) => {
+//           state.drawSettings.landmarkSize = size;
+//         })
+//       ),
+//     setLandmarkColor: ({ topColor, bottomColor }) =>
+//       set(
+//         produce((state: any) => {
+//           if (topColor) state.drawSettings.landmarkColor[0] = topColor;
+//           if (bottomColor) state.drawSettings.landmarkColor[1] = bottomColor;
+//         })
+//       ),
+//   }))
+// );
